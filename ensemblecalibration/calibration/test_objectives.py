6ea52db7e2477a07ec36f5429ccbe4f608c96967
mkjuergens <mira.kjuergens@gmail.com>
2023-03-14 14:08:16 +0100

    bugfix in test-objectives.py: add option to calculate P_bar for different dimensions of the weight vector/matrix

--- ensemblecalibration/calibration/test_objectives.py
+++ ensemblecalibration/calibration/test_objectives.py
@@ -6,15 +6,15 @@ from ensemblecalibration.calibration.calibration_measures import skce_ul_arr, sk
 from ensemblecalibration.calibration.distances import tv_distance
 
 def calculate_pbar(weights_l: np.ndarray, P: np.ndarray, reshape: bool = True):
-    """calculate convex combination of a weight matrix of shape (N,M) and a tensor of point predictions
+    """calculate convex combination of a weight matrix of shape (N,M) or (M,) and a tensor of point predictions
         of shape (N, M, K) such that the new matrix contains a point predictions for each instance and is 
         of shape (N, K).
     Parameters
     ----------
     weights_l : np.ndarray
-        weight matrix of shape (N, M)
+        weight matrix of shape (N, M) or of shape (M,)
     P : np.ndarray
-        tensor of point predcitions for each isntance for each predcitor, shape (N,M,K)
+        tensor of point predcitions for each instance for each predcitor, shape (N,M,K)
 
     Returns
     -------
@@ -22,15 +22,19 @@ def calculate_pbar(weights_l: np.ndarray, P: np.ndarray, reshape: bool = True):
         matrix containinng one new prediction for each instance, shape (N, K)
     """
 
-    n_rows = P.shape[0]
-    if reshape:
-        assert len(weights_l) % n_rows == 0, " weight vector needs to be a multiple of the number of rows"
-        weights_l = weights_l.reshape(n_rows, -1)
+    if weights_l.ndim == 2:
+        n_rows = P.shape[0]
+        if reshape:
+            assert len(weights_l) % n_rows == 0, " weight vector needs to be a multiple of the number of rows"
+            weights_l = weights_l.reshape(n_rows, -1)
 
-    assert weights_l.shape[0] == P.shape[0], " numer of samples need to be the same for P and weights_l"
-    assert weights_l.shape[1] == P.shape[1], " numer of ensemble members need to be the same for P and weights_l"
+        assert weights_l.shape[0] == P.shape[0], " numer of samples need to be the same for P and weights_l"
+        assert weights_l.shape[1] == P.shape[1], " numer of ensemble members need to be the same for P and weights_l"
 
-    P_bar = (weights_l[:,:,np.newaxis]*P).sum(-2) # sum over second axis to get diagonal elements
+        P_bar = (weights_l[:,:,np.newaxis]*P).sum(-2) # sum over second axis to get diagonal elements
+
+    elif weights_l.ndim == 1:
+        P_bar = np.matmul(np.swapaxes(P,1,2),weights_l)
 
     return P_bar
 
